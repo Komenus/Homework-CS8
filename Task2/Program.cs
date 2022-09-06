8eb70b9caee7282f8d55b61215c30b4f0f3a63ba
dmitriyzheleznyak <komenus@gmail.com>
2022-09-06 16:39:50 +0300

    all tasks done

---
+++ Task2/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+Console.Clear();
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine());
+// Console.Write("Введите количество столбцов массива: ");
+int columns = rows + 1;
+int[,] array = GetArray(rows, columns, 1, 10);
+PrintArray(array);
+Console.WriteLine();
+Console.Write($"Номер строки с наименьшей суммой значений {CountArray(array) +1} : ");
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+
+        }
+    }
+    return result;
+}
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        { Console.Write($"{inArray[i, j]} "); }
+        Console.WriteLine();
+    }
+}
+int CountArray(int[,] array)
+{   
+    int minsum = 0;
+    int row = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+    minsum = minsum + array[0, i];
+    int sum = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum = sum + array[i,j]; 
+        }
+        if ( minsum > sum ) 
+        {
+            minsum = sum;
+            row = i;
+        }
+    }
+    return row;
+}    
---
+++ Task3/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+Console.Clear();
+Console.Write("Введите количество строк  массива: ");
+int rows = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine());;
+int[,] array = GetArray(rows, columns, 1, 10);
+int[,] array1 = GetArray(rows, columns, 1, 10);
+PrintArray(array);
+Console.WriteLine();
+PrintArray(array1);
+Console.WriteLine();
+multArray(array, array1);
+PrintArray(multArray(array, array1));
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+
+        }
+    }
+    return result;
+}
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        { Console.Write($"{inArray[i, j]} "); }
+        Console.WriteLine();
+    }
+}
+int [,] multArray (int [,] array, int [,] array1) 
+{
+    int [,] array2 = new int [array.GetLength(0), array1.GetLength(1)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array1.GetLength(1); j++)
+        {
+            for (int k = 0; k <array.GetLength(1); k++)
+            {
+                array2 [i,j] +=  array [i,k] * array1 [k,j];
+            }
+
+        }
+    }
+    return array2;
+}
